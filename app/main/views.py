518f37598ff9983abdba35a888b974835081c8b4
kevin-kili <kilikevin435@gmail.com>
2020-05-13 10:37:04 +0300

    added update.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
 from . import main
 from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
+from .forms import ReviewForm,UpdateProfile
+from .. import db
 from flask_login import login_required
 from ..models import Review
 
@@ -50,6 +52,15 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
@@ -65,3 +76,22 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
